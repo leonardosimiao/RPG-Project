7fd6df03b945d887ade5804466e7b1f636bc4a56
leonardosimiao <leonardosimiaol@gmail.com>
2024-11-17 11:48:18 -0300

    feat: implement enemy damage

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -8,6 +8,7 @@ namespace RPG.Combat
     {
         [SerializeField] float weaponRange = 2.5f;
         [SerializeField] float attackCoolDown = 1f;
+        [SerializeField] float weaponDamage = 5f;
         
         Transform target;
         float timeSinceLastAttack = 0;
@@ -38,6 +39,7 @@ namespace RPG.Combat
         {
             if (timeSinceLastAttack > attackCoolDown)
             {
+                // This will trigger the Hit() event;
                 GetComponent<Animator>().SetTrigger("attack");
                 timeSinceLastAttack = 0;
             }
@@ -46,7 +48,8 @@ namespace RPG.Combat
         // Animation Event called on Unity, required for attack synchronization.
         private void Hit()
         {
-
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
 
         public void Attack(CombatTarget combatTarget)
