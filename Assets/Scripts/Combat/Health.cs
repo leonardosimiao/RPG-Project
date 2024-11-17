64f4ffcd0eb17729a06c2c36a8b9ea3d24ffa6d3
leonardosimiao <leonardosimiaol@gmail.com>
2024-11-17 20:21:30 -0300

    feat: implement death animation

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -4,12 +4,23 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour 
     {
-        [SerializeField] float health = 100f;
+        [SerializeField] float healthPoints = 100f;
+        bool isAlive = true;
 
         public void TakeDamage(float damage)
         {
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            if (healthPoints == 0 && isAlive)
+            {
+                Die();
+            }
+            
+        }
+
+        private void Die()
+        {
+            GetComponent<Animator>().SetTrigger("die");
+            isAlive = false;
         }
     }
 }
